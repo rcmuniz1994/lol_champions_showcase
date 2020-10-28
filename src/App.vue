1e6bfc397676a5fa7ce35afe18556e62963a3047
Rodrigo Muniz <rcmuniz51@gmail.com>
2020-10-27 23:47:27 -0300

    bootstrap project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img class="logo" alt="League of Legends logo" src="./assets/logo.png">
+  <ShowcaseTitle msg="LoL Champions Showcase"/>
+</template>
+
+<script>
+import ShowcaseTitle from './components/ShowCaseTitle.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ShowcaseTitle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  height: 128px;
+  width: 128px;
+}
+</style>
